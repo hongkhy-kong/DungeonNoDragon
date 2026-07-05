64c0357dcd2496f01e5f7a0f68491fd4acf64b3a
Suntei <sleang2@paragoniu.edu.kh>
2026-07-05 19:37:44 +0700

    Added main menu and Setting UI

---
+++ Scence/main_menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+func _ready():
+	$VBoxContainer/Start.grab_focus()
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://Map/level1_cave.tscn")
+
+func _on_setting_pressed():
+	# TODO: Open the settings menu res://Scence/setting.tscn
+	get_tree().change_scene_to_file("res://Scence/setting.tscn")
+	print("Settings button pressed")
+
+func _on_exit_pressed():
+	get_tree().quit()
